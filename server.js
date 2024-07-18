2f51fcf0a6dc9ceb843384ae7b96eb628a032506
Aleksandar Pavicevic <aleksandar.pavicevic.web@gmail.com>
2024-07-18 22:05:25 +0200

    worked on connecting to mongodb

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const MongoClient = require("mongodb").MongoClient;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri = "";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -19,4 +21,25 @@ app.listen(5000, function () {
   console.log(__dirname);
 });
 
-MongoClient.connect("mongodb-connection-string", (err, client) => {});
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
